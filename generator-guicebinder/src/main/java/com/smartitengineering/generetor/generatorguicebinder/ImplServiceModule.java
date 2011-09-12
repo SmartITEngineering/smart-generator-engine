1e43488f7102e7bc0a6da65de57ca976e8c623cd
Saumitra Chowdhury <saumitra@smartitengineering.com>
2011-09-12 10:31:18 +0600

    GuiceBind leass created
    Signed-off-by: Saumitra Chowdhury <saumitra@smartitengineering.com>

---
+++ generator-guicebinder/src/main/java/com/smartitengineering/generetor/generatorguicebinder/ImplServiceModule.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.generetor.generatorguicebinder;
+
+import com.google.inject.AbstractModule;
+
+/**
+ *
+ * @author saumitra
+ */
+public class ImplServiceModule extends AbstractModule{
+
+  @Override
+  protected void configure() {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+}

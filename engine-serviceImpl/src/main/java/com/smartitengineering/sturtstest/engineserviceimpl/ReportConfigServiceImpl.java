a365c4125059e5c945cafb49cf3661365a3ce30a
Saumitra Chowdhury <saumitra@smartitengineering.com>
2011-09-12 10:28:55 +0600

    ReportConfigServiceImpl Have been Created
    Signed-off-by: Saumitra Chowdhury <saumitra@smartitengineering.com>

---
+++ engine-serviceImpl/src/main/java/com/smartitengineering/sturtstest/engineserviceimpl/ReportConfigServiceImpl.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smartitengineering.sturtstest.engineserviceimpl;
+
+import com.smartitengineering.engine.domain.ReportConfig;
+import com.smartitengineering.engine.service.ReportConfigService;
+/**
+ *
+ * @author saumitra
+ */
+public class ReportConfigServiceImpl implements ReportConfigService {
+  
+  @Override
+  public void save(ReportConfig reportConfig) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  @Override
+  public void delete(ReportConfig reportConfig) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  @Override
+  public void update(ReportConfig reportConfig) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+}

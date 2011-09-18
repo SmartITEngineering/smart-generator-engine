d4690a4b32347a19d0d3e4463e84ec4b65bee68e
Saumitra Chowdhury <saumitra@smartitengineering.com>
2011-09-18 12:20:11 +0600

    Client added
    Signed-off-by: Saumitra Chowdhury <saumitra@smartitengineering.com>

---
+++ engine-webservice-client/src/main/java/com/smartitengineering/generetor/enginewebserviceclient/Api/ConfigResource.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smartitengineering.generetor.enginewebserviceclient.Api;
+
+import com.smartitengineering.util.rest.client.WritableResource;
+import org.apache.abdera.model.Feed;
+
+/**
+ *
+ * @author saumitra
+ */
+public interface ConfigResource extends WritableResource<Feed> {
+
+  public void updateConfig();
+
+  public void deleteConfig();
+}
---
+++ engine-webservice-client/src/main/java/com/smartitengineering/generetor/enginewebserviceclient/Api/CongigsResource.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smartitengineering.generetor.enginewebserviceclient.Api;
+
+import com.smartitengineering.util.rest.client.WritableResource;
+import org.apache.abdera.model.Feed;
+import java.net.URI;
+import java.util.Collection;
+
+/**
+ *
+ * @author saumitra
+ */
+public interface CongigsResource extends WritableResource<Feed> {
+
+  public void createConfig(URI configUri);
+
+  public Collection<ConfigResource> getAllConfig();
+}

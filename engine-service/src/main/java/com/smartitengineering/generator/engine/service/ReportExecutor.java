b9f05e76972069ac252496348254a0bd9fb3c90b
Imran M Yousuf <imyousuf@smartitengineering.com>
2011-09-19 09:43:48 +0600

    Add API documentation for report executor
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- engine-service/src/main/java/com/smartitengineering/generator/engine/service/ReportExecutor.java
+++ engine-service/src/main/java/com/smartitengineering/generator/engine/service/ReportExecutor.java
@@ -13,5 +13,11 @@ import java.util.Map;
  */
 public interface ReportExecutor {
 
+  /**
+   * A API to be implemented in scripting languages by report config sources. Its soul purpose is to execute processes
+   * required for generating a report and creating a content that can hold the output.
+   * @param params The parameters configured in report config
+   * @return A CMS Content which is of a type that is instance of Report configured by this engine
+   */
   public Content createReport(Map<String, String> params);
 }

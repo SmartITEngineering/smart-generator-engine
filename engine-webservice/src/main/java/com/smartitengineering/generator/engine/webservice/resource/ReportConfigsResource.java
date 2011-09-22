abda0ef1d96f250213a2ae554e9ca96f483cfc8e
Imran M Yousuf <imyousuf@smartitengineering.com>
2011-09-22 18:33:17 +0600

    Fix entry to report configs resource
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- engine-webservice/src/main/java/com/smartitengineering/generator/engine/webservice/resource/ReportConfigsResource.java
+++ engine-webservice/src/main/java/com/smartitengineering/generator/engine/webservice/resource/ReportConfigsResource.java
@@ -7,6 +7,7 @@ package com.smartitengineering.generator.engine.webservice.resource;
 import com.smartitengineering.generator.engine.service.ReportConfigFilter;
 import com.smartitengineering.generator.engine.webservice.domain.ReportConfig;
 import com.smartitengineering.generator.engine.service.factory.Services;
+import com.smartitengineering.generator.engine.webservice.adapter.ReportConfigAdapterHelper;
 import com.smartitengineering.util.bean.adapter.GenericAdapterImpl;
 import com.smartitengineering.util.rest.atom.server.AbstractResource;
 import java.util.Collection;
@@ -16,8 +17,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
@@ -33,25 +32,23 @@ import org.apache.abdera.model.Link;
  *
  * @author saumitra
  */
-@Path("/reportconfigs")
 public class ReportConfigsResource extends AbstractResource {
 
   @Context
   private HttpServletRequest servletRequest;
   private GenericAdapterImpl<ReportConfig, com.smartitengineering.generator.engine.domain.ReportConfig> adapter;
-  @DefaultValue("10")
-  @QueryParam("count")
-  private Integer count;
 
   public ReportConfigsResource() {
     adapter = new GenericAdapterImpl<ReportConfig, com.smartitengineering.generator.engine.domain.ReportConfig>();
-    //adapter.setHelper(new ReportConfigAdapterHelper());
+    adapter.setHelper(new ReportConfigAdapterHelper());
   }
 
   @GET
   @Produces(MediaType.APPLICATION_ATOM_XML)
-  public Response get(Integer index, @PathParam("startDate") Date startDate, @PathParam("endDate") Date endDate, @PathParam(
-      "nameLike") String nameLike) {
+  public Response get(@DefaultValue("10") @QueryParam("count") Integer count,
+                      @DefaultValue("0") @QueryParam("start") Integer index,
+                      @QueryParam("startDate") Date startDate, @QueryParam("endDate") Date endDate,
+                      @QueryParam("nameLike") String nameLike) {
     ResponseBuilder responseBuilder = Response.status(Status.OK);
     Feed atomFeed = getFeed("ReportConfigs", new Date());
     ReportConfigFilter reportConfigFilter = new ReportConfigFilter();
@@ -76,38 +73,6 @@ public class ReportConfigsResource extends AbstractResource {
         atomFeed.addEntry(reportConfigEntry);
       }
     }
-    return responseBuilder.build();
-  }
-
-  @GET
-  @Produces(MediaType.TEXT_HTML)
-  @Path("/{index}")
-  public Response getHtmlIndex(@PathParam("index") Integer index, @PathParam("startDate") Date startDate, @PathParam(
-      "endDate") Date endDate, @PathParam("nameLike") String nameLike) {
-    ResponseBuilder responseBuilder = Response.status(Status.OK);
-    if (count == null) {
-      count = 10;
-    }
-    ReportConfigFilter reportConfigFilter = new ReportConfigFilter();
-    reportConfigFilter.setNameLike(nameLike);
-    reportConfigFilter.setScheduleRangeStart(startDate);
-    reportConfigFilter.setScheduleRangeEnd(endDate);
-    Collection<com.smartitengineering.generator.engine.domain.ReportConfig> configs = Services.getInstance().
-        getReportConfigService().searchConfigs(reportConfigFilter);
-    servletRequest.setAttribute("index", index);
-    servletRequest.setAttribute("total", Services.getInstance().getReportConfigService().searchConfigs(
-        reportConfigFilter).size());
-    servletRequest.setAttribute("count", count);
-//    Viewable view = new Viewable("",adapter.convertInversely(configs.toArray(new PersistentReportConfig[configs.size()])));
-//    responseBuilder.entity(view);
-    return responseBuilder.build();
-
-  }
-
-  @GET
-  @Produces(MediaType.TEXT_HTML)
-  public Response getHtml() {
-    ResponseBuilder responseBuilder = Response.status(Status.OK);
     return responseBuilder.build();
   }
 
